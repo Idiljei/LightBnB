1b3c5e81d1ed0358ebc7c35ae943884335e748ce
Idiljei <i.jeilani@gmail.com>
2021-02-03 21:06:54 +0000

    5_all_my_reservations.sql

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,32 @@
+-- Show all reservations for a user.
+
+-- Select all columns from the reservations table, all columns from the properties table, 
+--and the average rating of the property.
+-- The reservations will be for a single user, so use 1 for the user_id.
+-- Order the results from the earliest start_date to the most recent start_date.
+-- These will end up being filtered by either "Upcoming Reservations" or "Past Reservations", 
+--so only get reservations where the end_date is in the past.
+-- Use now()::date to get today's date.
+-- Limit the results to 10.
+
+-- WRONG-- Go over it later and review 
+-- SELECT reservations.*, properties.*, AVG(property_reviews.rating)
+-- FROM reservations 
+-- JOIN properties ON reservations.properties_id = property.id
+-- JOIN property_reviews ON reservations.id = reservation_id
+-- JOIN users ON users.id = reservations.guest_id
+-- WHERE users.id = '1'
+-- GROUP BY reservations
+-- ORDER BY reservations.start_date 
+-- LIMIT 10; 
+
+
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
